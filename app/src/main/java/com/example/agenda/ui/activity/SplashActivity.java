696ee8df8e391b24d49f603567c637f40c6983ec
possante-camila <camila.possan@objectdge.com>
2023-06-20 19:14:52 -0300

    Code fix, a tela de login tinha parado de aparecer depois da splash

--- app/src/main/java/com/example/agenda/ui/activity/SplashActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.agenda.ui.activity.model;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +11,6 @@ import com.example.agenda.ui.activity.Login;
 import com.example.agenda.R;
 
 public class SplashActivity extends AppCompatActivity {
-
     private static final long SPLASH_DELAY = 2000; // Tempo de exibição da tela de splash em milissegundos
 
     @Override
@@ -18,12 +18,12 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, Login.class); // Redireciona para a tela de login
+                Intent intent = new Intent(SplashActivity.this, Login.class);
                 startActivity(intent);
-                finish(); // Finaliza a SplashActivity para que ela não seja retornada quando o usuário pressionar o botão de voltar
+                finish();
             }
         }, SPLASH_DELAY);
     }
